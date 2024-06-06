f59c5977c7c138553342a560f1d6efd15541c06f
julia mirante08 <julia.mirante@eaportal.org>
2024-06-06 01:12:01 +0000

    adicionada a funcao de inserir o resultado na tela

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,8 +1,8 @@
 import { verificarTema, trocarTema} from "../../helpers/tema-helper.js"
 
-const assunto = localStorage.getItem("assunto")
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
 
 let quiz = {}
 let pontos = 0
